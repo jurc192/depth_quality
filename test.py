9d256e636fc4a60e86d7a0751509a5fe3847c340
jurc192 <vidmarjur@gmail.com>
2020-04-29 17:58:40 +0200

    Setting sensor options works

--- test.py
+++ test.py
@@ -5,32 +5,26 @@ import cv2
 
 
 if __name__ == "__main__":
+
+    # Initialize realsense stuff
     pipeline = rs.pipeline()
     config = rs.config()
-    config.enable_stream(rs.stream.depth, 424, 240, rs.format.z16)
-
+    config.enable_stream(rs.stream.depth, 1280, 720)
     profile = config.resolve(pipeline)
-
     device = profile.get_device()
-    sensor = device.first_depth_sensor()
+    pipeline.start()
 
-    # What can this depth_sensor do?
-    # sensor.profiles -> list of supported stream profiles (fps+res+etc.)
-    # sensor.get_supported_options() ->
-    #   option.exposure,
-    #   option.gain,
-    #   option.enable_auto_exposure,
-    #   option.visual_preset,
-    #   option.laser_power,
-    #   option.emitter_enabled,
-    #   option.frames_queue_size,
-    #   option.asic_temperature,
-    #   option.error_polling_enabled,
-    #   option.projector_temperature,
-    #   option.output_trigger_enabled,
-    #   option.depth_units,
-    #   option.stereo_baseline,
-    #   option.inter_cam_sync_mode,
-    #   option.emitter_on_off,
-    #   option.global_time_enabled
+    frames = pipeline.wait_for_frames()
+    depth_frame = frames.get_depth_frame()
+
+    sensor = device.first_depth_sensor()
+    pipeline.stop()
+    print(sensor.get_option(rs.option.laser_power))
+    sensor.set_option(rs.option.laser_power, 50)
+    print(sensor.get_option(rs.option.laser_power))
+    pipeline.start()
+
+    frames = pipeline.wait_for_frames()
+    depth_frame = frames.get_depth_frame()
+
 
