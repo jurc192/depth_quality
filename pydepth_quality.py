ff61ffddcd0528bdda1a323536ec117e79fd41cc
jurc192 <vidmarjur@gmail.com>
2020-04-29 16:37:54 +0200

    Getting depth sensor WIP

--- pydepth_quality.py
+++ pydepth_quality.py
@@ -1,48 +1,25 @@
 #
 #   Custom depth quality measurement tool
 #
-# Usage: python pydepth_quality.py <input_file.ply> 
 
-# What do we want?
-# - live preview
-# - button press
-# - taking a depth image frame
+# TODO:
+# OK - live preview
+# OK - button press
+# OK - taking a depth image frame
 # - calculating best fit plane
 # - calculating RMSE
 # - displaying results
-
-# Use Open3D library for vizualization, start with reading .ply from file
-
 
 import pyrealsense2 as rs
 import open3d as o3d
 import numpy as np
 import cv2
-import sys
 
-# if len(sys.argv) < 2:
-#     print("Usage: ./pydepth_quality.py <pcl file>")
-#     sys.exit(1)
-
-# pcl = o3d.io.read_point_cloud(sys.argv[1])
-# if (pcl):
-#     o3d.visualization.draw_geometries([pcl], window_name='Point cloud visualization', width=800, height=600)
-# else:
-#     print("Bad input file")
 
 
 def display_pointcloud(depth_image, intrinsics):
 
-    # depth (open3d.geometry.Image) –
-    # intrinsic (open3d.camera.PinholeCameraIntrinsic) –
-    # extrinsic (numpy.ndarray[float64[4, 4]], optional) – array([[1., 0., 0., 0.], [0., 1., 0., 0.], [0., 0., 1., 0.], [0., 0., 0., 1.]])
-    # depth_scale (float, optional, default=1000.0) –
-    # depth_trunc (float, optional, default=1000.0) –
-    # stride (int, optional, default=1) –
-
-    print(intrinsics)
-    print(depth_image)
-
+    # Convert rs2 depthmap into o3d depthmap
     depth_image = o3d.geometry.Image(depth_image)
     intr = o3d.camera.PinholeCameraIntrinsic(
         intrinsics.width,
@@ -52,18 +29,14 @@ def display_pointcloud(depth_image, intrinsics):
         intrinsics.ppx,
         intrinsics.ppy
     )
-
-    # depth_image = o3d.geometry.Image(depth_image)
     o3ddepth = o3d.geometry.PointCloud.create_from_depth_image(depth_image, intr)
+
+    # calculate best-fit plane
+
     o3d.visualization.draw_geometries([o3ddepth], width=800, height=600)
 
 
-
-
-
-
-
-if __name__ == "__main__":
+def main():
 
     # Configure depth and color streams
     pipeline = rs.pipeline()
@@ -77,7 +50,6 @@ if __name__ == "__main__":
     try:
         while True:
 
-            # Wait for a coherent pair of frames: depth and color
             frames = pipeline.wait_for_frames()
             depth_frame = frames.get_depth_frame()
             color_frame = frames.get_color_frame()
@@ -90,23 +62,28 @@ if __name__ == "__main__":
 
             # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
             depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
-
-            # Stack both images horizontally
             images = np.hstack((color_image, depth_colormap))
 
-            # Show images
             cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
             cv2.imshow('RealSense', images)
             
             key = cv2.waitKey(0)
-            if key == ord('q') or key == 27:
+            if key == ord('q') or key == 27:    # esc
                 break
-            elif key == 13:
-                print("Getting video stream intrinsics...")
+
+            elif key == 13:     # enter
+                cv2.destroyAllWindows()
                 intr = pipeline.get_active_profile().get_stream(rs.stream.depth).as_video_stream_profile().get_intrinsics()
                 display_pointcloud(depth_image, intr)
 
-    finally:
+    
+    except:
+        print(e)
 
-        # Stop streaming
+    finally:
         pipeline.stop()
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ test.py
@@ -0,0 +1,35 @@
+import pyrealsense2 as rs
+import open3d as o3d
+import numpy as np
+import cv2
+
+
+if __name__ == "__main__":
+    pipeline = rs.pipeline()
+    config = rs.config()
+    config.enable_stream(rs.stream.depth)
+    profile = config.resolve(pipeline)
+
+    device = profile.get_device()
+    sensor = device.first_depth_sensor()
+
+    # What can this depth_sensor do?
+    # sensor.profiles -> list of supported stream profiles (fps+res+etc.)
+    # sensor.get_supported_options() ->
+    #   option.exposure,
+    #   option.gain,
+    #   option.enable_auto_exposure,
+    #   option.visual_preset,
+    #   option.laser_power,
+    #   option.emitter_enabled,
+    #   option.frames_queue_size,
+    #   option.asic_temperature,
+    #   option.error_polling_enabled,
+    #   option.projector_temperature,
+    #   option.output_trigger_enabled,
+    #   option.depth_units,
+    #   option.stereo_baseline,
+    #   option.inter_cam_sync_mode,
+    #   option.emitter_on_off,
+    #   option.global_time_enabled
+
