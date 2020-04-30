8d0dead6b75869ae90d7ed09543a836937a79825
jurc192 <vidmarjur@gmail.com>
2020-04-30 18:34:34 +0200

    capture_depthframe works. Resolution parameter needs to be fixed

--- test.py
+++ test.py
@@ -3,19 +3,6 @@ import open3d as o3d
 import numpy as np
 import cv2
 
-
-supported_resolutions = [
-    (1280, 720),
-    (848, 480),
-    (640, 480),
-    (640, 360),
-    (480, 270)
-]
-
-exposures = [6500, 7000, 7500, 8000, 8500, 9000, 9500, 10000]
-laser_powers = [i for i in range(0, 360, 30)]
-
-
 class ParameterExplorer:
     
     def __init__(self):
@@ -23,7 +10,7 @@ class ParameterExplorer:
         self.config   = rs.config()
 
 
-    def capture_depthframe(self, resolution, exposure, laser_power, depth_preset=1):
+    def capture_depthframe(self, resolution=(848, 480), exposure=8500, laser_power=240, depth_preset=1):
         """ Capture a single depth frame with given parameter settings
         
         resolution  - (width, height) pixels
@@ -39,55 +26,56 @@ class ParameterExplorer:
         if (self.config.can_resolve(self.pipeline) == False):
             print("Resolution not supported")
             return
-        self.sensor = config.resolve(self.pipeline).get_device().first_depth_sensor()
 
-        # Set exposure
+        # Enable stream
+        self.sensor = self.config.resolve(self.pipeline).get_device().first_depth_sensor()
+
+        # Set exposure, laser_power and depth_preset
         if (exposure == 0):
-            self.sensor.set_option(rs.option.auto_exposure, True)
+            self.sensor.set_option(rs.option.enable_auto_exposure, True)
         else:
             self.sensor.set_option(rs.option.exposure, exposure)
-
-        # Set laser power
         self.sensor.set_option(rs.option.laser_power, laser_power)
-
-        # Set depth preset
         self.sensor.set_option(rs.option.visual_preset, depth_preset)
 
 
-    @staticmethod
-    def get_frame_metadata(frame):
-        metadata = {}
-        metadata['resolution (px)'] = f"{frame.get_width()} x {frame.get_height()} px"
-        metadata['autoexposure']  = frame.get_frame_metadata(rs.frame_metadata_value.auto_exposure)
-        metadata['exposure (ms)'] = frame.get_frame_metadata(rs.frame_metadata_value.actual_exposure) # docs say ms, but not really sure
-        metadata['laser power (mW)']  = frame.get_frame_metadata(rs.frame_metadata_value.frame_laser_power)
-        return metadata
+        # Get a depth frame
+        self.pipeline.start()
+        frame = self.pipeline.wait_for_frames().get_depth_frame()
+        self.pipeline.stop()
+
+        return frame
+
+
+def get_frame_metadata(frame):
+    metadata = {}
+    metadata['resolution (px)'] = f"{frame.get_width()} x {frame.get_height()} px"
+    metadata['autoexposure']  = frame.get_frame_metadata(rs.frame_metadata_value.auto_exposure)
+    metadata['exposure (ms)'] = frame.get_frame_metadata(rs.frame_metadata_value.actual_exposure) # docs say ms, but not really sure
+    metadata['laser power (mW)']  = frame.get_frame_metadata(rs.frame_metadata_value.frame_laser_power)
+    return metadata
 
 
 if __name__ == "__main__":
 
-    # Initialize realsense stuff
-    pipeline = rs.pipeline()
-    config = rs.config()
-    config.enable_stream(rs.stream.depth, 1280, 720)
-    profile = config.resolve(pipeline)
-    device = profile.get_device()
-    pipeline.start()
 
-    frames = pipeline.wait_for_frames()
-    depth_frame = frames.get_depth_frame()
+    supported_resolutions = [
+        (1280, 720),
+        (848, 480),
+        (640, 480),
+        (640, 360),
+        (480, 270)
+    ]
 
-    sensor = device.first_depth_sensor()
-    pipeline.stop()
-    print(sensor.get_option(rs.option.laser_power))
-    sensor.set_option(rs.option.laser_power, 50)
-    print(sensor.get_option(rs.option.laser_power))
-    pipeline.start()
+    exposures = [6500, 7000, 7500, 8000, 8500, 9000, 9500, 10000]
+    # laser_powers = [i for i in range(0, 360, 30)]
 
-    frames = pipeline.wait_for_frames()
-    depth_frame = frames.get_depth_frame()
 
-    print(get_metadata(depth_frame))
-    
-
+    cam = ParameterExplorer()
+    frame = cam.capture_depthframe()
+    print(get_frame_metadata(frame))
+    frame = cam.capture_depthframe((1280, 720), 0, 150, 1)
+    print(get_frame_metadata(frame))
+    frame = cam.capture_depthframe((1280, 720), 8500, 150, 1)
+    print(get_frame_metadata(frame))
 
