492bf6f46cb487f58f642cc8ba5ce681b2df2853
jurc192 <vidmarjur@gmail.com>
2020-05-02 17:37:35 +0200

    pydepth_quality now previews color, depth and ir streams

--- pydepth_quality.py
+++ pydepth_quality.py
@@ -14,6 +14,7 @@ import pyrealsense2 as rs
 import open3d as o3d
 import numpy as np
 import cv2
+import traceback
 
 
 
@@ -43,45 +44,53 @@ def main():
     config = rs.config()
     config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
     config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+    config.enable_stream(rs.stream.infrared, 1, 640,480, rs.format.y8, 30)
+    config.enable_stream(rs.stream.infrared, 2, 640,480, rs.format.y8, 30)
 
     # Start streaming
     pipeline.start(config)
 
-    try:
-        while True:
+    # try:
+    while True:
 
-            frames = pipeline.wait_for_frames()
-            depth_frame = frames.get_depth_frame()
-            color_frame = frames.get_color_frame()
-            if not depth_frame or not color_frame:
-                continue
+        frames = pipeline.wait_for_frames()
+        depth_frame = frames.get_depth_frame()
+        color_frame = frames.get_color_frame()
+        irLeft_frame = frames.get_infrared_frame(1)
+        irRight_frame = frames.get_infrared_frame(2)
 
-            # Convert images to numpy arrays
-            depth_image = np.asanyarray(depth_frame.get_data())
-            color_image = np.asanyarray(color_frame.get_data())
+        # Convert images to numpy arrays
+        depth_image = np.asanyarray(depth_frame.get_data())
+        color_image = np.asanyarray(color_frame.get_data())
+        irLeft_frame = np.asanyarray(irLeft_frame.get_data())
+        irRight_frame = np.asanyarray(irRight_frame.get_data())
 
-            # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
-            depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
-            images = np.hstack((color_image, depth_colormap))
+        # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
+        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+        imagesTop    = np.hstack((color_image, depth_colormap))
+        imagesBottom = cv2.cvtColor(np.hstack((irLeft_frame, irRight_frame)), cv2.COLOR_GRAY2BGR)
+        images = np.vstack((imagesTop, imagesBottom))
 
-            cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
-            cv2.imshow('RealSense', images)
-            
-            key = cv2.waitKey(0)
-            if key == ord('q') or key == 27:    # esc
-                break
+        cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
+        cv2.imshow('RealSense', images)
+        
+        key = cv2.waitKey(0)
+        if key == ord('q') or key == 27:    # esc
+            break
 
-            elif key == 13:     # enter
-                cv2.destroyAllWindows()
-                intr = pipeline.get_active_profile().get_stream(rs.stream.depth).as_video_stream_profile().get_intrinsics()
-                display_pointcloud(depth_image, intr)
+        elif key == 13:     # enter
+            cv2.destroyAllWindows()
+            intr = pipeline.get_active_profile().get_stream(rs.stream.depth).as_video_stream_profile().get_intrinsics()
+            display_pointcloud(depth_image, intr)
 
+        # pipeline.stop()
     
-    except:
-        print(e)
+    # except Exception as e:
+    #     print(e)
+    #     print(traceback.format_exc())
 
-    finally:
-        pipeline.stop()
+    # finally:
+    #     pipeline.stop()
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -16,10 +16,9 @@ def get_intrinsics(width=1280, height=720):
 
 
 def capture_depthframe(width=1280, height=720, exposure=0, laser_power=240, depth_preset=1):
-
     pipeline = rs.pipeline()
     config   = rs.config()
-
+    
     # Set resolution and check if it's valid
     config.enable_stream(rs.stream.depth, width, height)
     if (config.can_resolve(pipeline) == False):
@@ -42,30 +41,21 @@ def capture_depthframe(width=1280, height=720, exposure=0, laser_power=240, dept
             pipeline.wait_for_frames().get_depth_frame()
             sleep(0.2)
     frame = pipeline.wait_for_frames().get_depth_frame()
-    
     config.disable_all_streams()
     pipeline.stop()
-
     return frame
-
-
-def get_metadata(frame):
-    metadata = {}
-    metadata['resolution (px)'] = f"{frame.get_width()} x {frame.get_height()} px"
-    metadata['autoexposure']  = frame.get_frame_metadata(rs.frame_metadata_value.auto_exposure)
-    metadata['exposure (ms)'] = frame.get_frame_metadata(rs.frame_metadata_value.actual_exposure) # docs say ms, but not really sure
-    metadata['laser power (mW)']  = frame.get_frame_metadata(rs.frame_metadata_value.frame_laser_power)
-    return metadata
 
 
 def save_depth_raw(filename, depthmap):
     depthmap = np.asanyarray(depthmap.get_data())
     np.savetxt(filename, depthmap, fmt="%u")
 
+
 def save_depth_colorized(filename, depthmap):
     depthmap = np.asanyarray(depthmap.get_data())
     colorized = cv2.applyColorMap(cv2.convertScaleAbs(depthmap, alpha=0.03), cv2.COLORMAP_JET)
     cv2.imwrite(filename, colorized)
+
 
 def save_pointcloud(filename, depthmap, intrinsics):
     # Convert realsense depthmap and intrinsics into open3d formats
@@ -81,6 +71,15 @@ def save_pointcloud(filename, depthmap, intrinsics):
     )
     pointcloud = o3d.geometry.PointCloud.create_from_depth_image(depthmap, intr)
     o3d.io.write_point_cloud(filename, pointcloud)
+
+
+def get_metadata(frame):
+    metadata = {}
+    metadata['resolution (px)'] = f"{frame.get_width()} x {frame.get_height()} px"
+    metadata['autoexposure']  = frame.get_frame_metadata(rs.frame_metadata_value.auto_exposure)
+    metadata['exposure (ms)'] = frame.get_frame_metadata(rs.frame_metadata_value.actual_exposure) # docs say ms, but not really sure
+    metadata['laser power (mW)']  = frame.get_frame_metadata(rs.frame_metadata_value.frame_laser_power)
+    return metadata
 
 
 if __name__ == "__main__":
@@ -101,8 +100,8 @@ if __name__ == "__main__":
 
 
     res = (848, 480)
-    exposure = 9000
-    laserpower = 300
+    exposure = 8000
+    laserpower = 240
 
     print("Init")
     depthframe = capture_depthframe(*res, exposure, laserpower)
