00a642eba347f4ee1b52318c2513a2b76ddd92dc
jurc192 <vidmarjur@gmail.com>
2020-05-05 10:58:12 +0200

    Organize depth_analysis script, plane_fit_RMSE()

--- depth_analysis.py
+++ depth_analysis.py
@@ -7,52 +7,50 @@ import sys
 from sklearn import linear_model
 from math import sqrt
 
-def rmse(errors):
+
+def rms(errors):
     return np.sum((errors**2)) / len(errors)
+
 
 def dist_to_plane(x, y, z, a, b, c, d):
     return (a*x + b*y + c*z - d) / sqrt(a*a + b*b + c*c)
+
+
+def plane_fit_RMSE(points, depth_unit=0.0001):
+    
+    # Form the system of equations in matrix form: Aw = z
+    A = []
+    z = []
+    for p in points:
+        A.append([p[0], p[1], 1])
+        z.append(p[2])
+    
+    # Perform linear regression
+    reg = linear_model.LinearRegression()
+    reg.fit(A, z)
+
+    # Get plane formula
+    a = reg.coef_[0]
+    b = reg.coef_[1]
+    c = 1
+    d = reg.intercept_
+    
+    print(f"{a:.2f}x + {b:.2f}y + {c:.2f}z + {d:.2f} = 0")
+    
+    # Calculate distances
+    distances = []
+    for p in points:
+        distances.append(dist_to_plane(*p, a, b, c, d))
+
+    return rms(np.array(distances)) * depth_unit
 
 
 # Read pointcloud
 if len(sys.argv) < 2:
     print("Usage: ./depth_analysis.py <input_file.ply>")
     sys.exit()
+
 points = np.asanyarray(o3d.io.read_point_cloud(sys.argv[1]).points)
 
-# Form the system of equations in matrix form: Aw = z
-A = []
-z = []
-for p in points:
-    A.append([p[0], p[1], 1])
-    z.append(p[2])
-
-# print(points)
-# print("\n")
-# print(np.matrix(A))
-# print("\n")
-# print(np.matrix(z).T)
-
-# Perform linear regression
-reg = linear_model.LinearRegression()
-reg.fit(A, z)
-
-# Get plane formula
-a = reg.coef_[0]
-b = reg.coef_[1]
-c = 1
-d = reg.intercept_
-
-print(f"{a:.2f}x + {b:.2f}y + {c:.2f}z + {d:.2f} = 0")
-
-
-# Calculate distances
-distances = []
-for p in points:
-    distances.append(dist_to_plane(*p, a, b, c, d))
-
-# print(min(distances))
-# print(max(distances))
-# print(f"RMSE: {rmse(np.array(distances)) * 100} um")
-print(f"RMSE: {rmse(np.array(distances)) * 0.0001} m")
+print(f"RMSE: {plane_fit_RMSE(points)} m")
 
