e3e0e10ab40d47db02d6f4a38d9ae9cb3b9154d6
jurc192 <vidmarjur@gmail.com>
2020-05-18 14:19:35 +0200

    Plotting both baseline and best

--- plotting/baseline.py
+++ plotting/baseline.py
@@ -18,12 +18,26 @@ for file in path.glob('*.csv'):
 data = np.array(data)
 
 # Calc stuff
-means = np.mean(data, axis=0)
-stddevs = np.std(data, axis=0)
-print(means)
-print(stddevs)
+means_base = np.mean(data, axis=0)
+stddevs_base = np.std(data, axis=0)
+print(means_base)
+print(stddevs_base)
+
+path = Path('../experiment9_best/results/')
+data = []
+for file in path.glob('*.csv'):
+    row = np.loadtxt(file)
+    data.append(row)
+data = np.array(data)
+
+# Calc stuff
+means_best = np.mean(data, axis=0)
+stddevs_best = np.std(data, axis=0)
+print(means_best)
+print(stddevs_best)
 
 # Plot
 distances = [200, 300, 400, 500, 600, 700]
-plt.errorbar(distances, means, stddevs, linestyle='-', marker='^')
+plt.errorbar(distances, means_base, stddevs_base, linestyle='-', marker='^')
+plt.errorbar(distances, means_best, stddevs_best, linestyle='-', marker='^')
 plt.show()
